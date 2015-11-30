00e9d633f635a09f1c8dff92530b02047ec8191c
st081422 <st081422@ew21-20.int.kcg.edu>
2015-11-30 16:46:26 +0900

    memo

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,47 @@
-var module = ons.bootstrap('app', ['onsen']);
-module.controller('AppController', function($scope){
-  $scope.goDetail = function(){
+// var module = ons.bootstrap('app', ['onsen']);
+var module = angular.module('app', ['onsen']);
+
+module.factory('MemoData', function(){
+  var memodata = {};
+  memodata.currentItem = {};
+  memodata.memos = [
+  {"title": "タイトル１", "memo": "メモ１", "time": "00:00:00"},
+  {"title": "タイトル２", "memo": "メモ２", "time": "00:00:00"},
+  {"title": "タイトル３", "memo": "メモ３", "time": "00:00:00"}
+  ];
+  return memodata;
+});
+
+module.controller('MainController',['MemoData', function(MemoData){
+  this.createMemo = function(){
+    app.navi.pushPage('page.html');
+    MemoData.currentItem = "";
+  };
+}]);
+
+module.controller('ListController', ['MemoData', function(MemoData){
+  this.goDetail = function(index){
+    MemoData.currentItem = this.memos[index];
     app.navi.pushPage('page.html');
   };
-});
-module.controller('PageController', function($scope){
-  ons.ready(function(){
+  this.memos = MemoData.memos;
+}]);
 
-  });
-});
-module.controller('listCtrl', function($scope){
-  $scope.memos = [
-  // {"title": "タイトル１", "memo": "メモ１"},
-  // {"title": "タイトル２", "memo": "メモ２"},
-  // {"title": "タイトル３", "memo": "メモ３"}
-  ];
-});
+module.controller('DetailController', ['MemoData', function(MemoData){
+  if(!MemoData.currentItem){
+    this.title = "";
+    this.content = "";
+    this.time = "";
+  }else{
+    this.title = MemoData.currentItem.title;
+    this.content = MemoData.currentItem.content;
+    this.time = MemoData.currentItem.time;
+  }
+  this.save = function(){
+    this.saveItem = {
+      "title": this.title,
+      "content": this.content,
+      "time": this.time
+    };
+  };
+}]);
